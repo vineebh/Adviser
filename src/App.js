e1de388a20e29fb1ac33171341c7e880d84a6c13
vineet <vineetbhandari5110@gmail.com>
2024-09-18 20:38:21 +0530

    adviser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [adv , setAdvice] = useState("Null");
+  const [day, setDay] = useState(-1);
+
+  async function getAdvice(){
+    const res= await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setDay((c)=>c+1);
+  }
+
+  useEffect(function(){
+    getAdvice();
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Advice for today is </h3>
+        
+        <h1>{adv}</h1>
+
+        <button className='btn' onClick={getAdvice}>Another one</button>
+        <Message day={day}/>
       </header>
     </div>
   );
+}
+
+function Message(props){
+  return (
+    <h3>Advice Count {props.day}</h3>
+  )
 }
 
 export default App;
